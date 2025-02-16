a949b79fe69fba9b1aeb885fc28acc3e99ac65e4
Saulo Barros <saulobarros1997@gmail.com>
2025-02-15 22:45:37 -0300

    remove: unnecessary lib

--- desafio_1/libs/KabumLibrary.py
+++
@@ -1,7 +0,0 @@
-from robot.api.deco import keyword
-
-class KabumLibrary:
-
-    @keyword("Validate Product Details")
-    def validate_product_details(self, product_name, expected_name):
-        assert product_name in expected_name, f"Expected {expected_name}, but got {product_name}"
